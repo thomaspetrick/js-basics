8438c7865ef1ec60eb68fbc18779d59d3748f34f
thomaspetrick <175740578+thomaspetrick@users.noreply.github.com>
2024-10-31 17:23:58 -0400

    messing with js

---
+++ javascript.js
@@ -0,0 +1,31 @@
+let name = "John";
+let surname = "Doe";
+
+console.log(name);
+console.log(surname);
+
+let age = 11;
+console.log(age);
+
+age = 54;
+console.log(age);
+
+const pi = 3.14;
+console.log(pi);
+
+console.log((3+2) - 76 * (1+1));
+
+console.log(23+97);
+console.log(10+11+12+13+14+15);
+console.log((4+6+9)/77);
+
+let a = 10
+console.log(a)
+console.log(a * 10)
+let b = 7 * a
+console.log(b)
+
+const max = 57;
+let actual = max - 13;
+let percentage = actual / max;
+console.log(percentage);
